f93f8bb54234a451e325a6793f81dbd015127555
oscarTematio <omtematio@gmail.com>
2019-05-04 21:52:31 +0200

    the route for the new line were added

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask,render_template
-
+from mocks import Post
 app=Flask(__name__)
 
+posts=Post.all()
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -9,10 +10,23 @@ def home():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+    
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/blog')
+def posts_index():
+    posts=Post.all()
+    return render_template("posts/index.html",posts=posts)
+
+@app.route('/blog/post/<int:id>')
+def show_posts(id): 
+    print(id)
+    post= Post.find(id)
+    post=posts[id-1]
+    return render_template("posts/show.html",post =post)
 
 @app.errorhandler(404)
 def page_not_found(error):
